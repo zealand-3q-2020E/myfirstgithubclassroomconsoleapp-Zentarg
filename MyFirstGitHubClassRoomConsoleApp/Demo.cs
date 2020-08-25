f496ed24a1376f8e325b279905029068cc97f1e9
Zentarg <apg.chili@gmail.com>
2020-08-25 13:08:36 +0200

    Exercise

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class Demo
+    {
+        public Demo(string name, string description, List<string> stringList)
+        {
+            Name = name;
+            Description = description;
+            StringList = stringList;
+        }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public List<string> StringList { get; set; }
+
+
+
+    }
+}
